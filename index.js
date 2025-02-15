1494204859c51975757fd81e48ca2fbf6e14e932
Trong Duc <trongduc22062000@gmail.com>
2025-02-15 13:42:58 +0200

    add cors

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ pool.connect((err) => {
   });
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (request, response) => {
